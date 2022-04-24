d352cb36c3b6a016dcdf202e2a4c71a84db451c5
rouanis5 <ouanis42@gmail.com>
2022-04-24 19:10:07 +0100

    + updateMap(), export it

--- src/js/map.js
+++ src/js/map.js
@@ -1,11 +1,17 @@
-import('mapbox-gl').then((mapboxgl) => {
-  const mapbox = mapboxgl;
-  mapbox.accessToken =
-    'pk.eyJ1Ijoid2FuaXMiLCJhIjoiY2wyZGNtZGkwMDkwejNjbnN0MXJ0MjNvNSJ9.Udb0eEdEwQCVadG5PJcoIg';
-  return new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+const map = import('mapbox-gl');
+
+function updateMap(lng, lat) {
+  map.then((mapboxgl) => {
+    const mapbox = mapboxgl;
+    mapbox.accessToken =
+      'pk.eyJ1Ijoid2FuaXMiLCJhIjoiY2wyZGNtZGkwMDkwejNjbnN0MXJ0MjNvNSJ9.Udb0eEdEwQCVadG5PJcoIg';
+    return new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [lng, lat], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
   });
-});
+}
+
+export default updateMap;
